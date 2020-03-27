aa6e72a0813c8473cbe1ee7bd5f296b97bf726ae
Santosh Pun <santosh@f1soft.com>
2020-03-27 21:18:43 +0545

    ref

--- inquiry/src/main/java/com/santosh/inquiry/InquiryApplication.java
+++ inquiry/src/main/java/com/santosh/inquiry/InquiryApplication.java
@@ -9,16 +9,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @Slf4j
 @SpringBootApplication(scanBasePackages = {"com.santosh"})
+@RestController
 public class InquiryApplication implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
     @Autowired
     private BookRepository bookRepository;
+
+    @GetMapping("/home")
+    public String home() {
+        return "Running inquiry service";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(InquiryApplication.class, args);
